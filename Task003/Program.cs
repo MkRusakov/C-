1528ebeb347ce1be1ffd19a3cdfea117e9383459
Mickey <mk.ru5@yandex.ru>
2022-08-04 21:38:38 +0300

    Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,8 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1) {Console.Write("Понедельник");}
+else  if (num == 2) {Console.Write("Вторник");}
+if (num == 3) {Console.Write("Среда");}
+else if (num == 4) {Console.Write("Четверг");}
+if (num == 5) {Console.Write("Пятница");}
+else if (num == 6) {Console.Write("Суббота");}
+if (num == 7) {Console.Write("Воскресенье");}
